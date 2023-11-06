fdad7f5adb0cdc35a5cb5cde1926fc198542d911
Sefa Gür <sefa_guer@hotmail.com>
2023-11-06 10:12:15 +0100

    chicken position and reload clearing canvas

--- models/world.class.js
+++ models/world.class.js
@@ -5,16 +5,28 @@ class World {
         new Chicken(),
         new Chicken(),
     ]
-
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
         this.ctx.drawImage(this.characters.img, this.characters.x, this.characters.y, this.characters.width, this.characters.height)
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
+        });
+
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw()
+        });
     }
 }
