5840e275144c612051748a73dc1266f7a86c5f76
Sefa Gür <sefa_guer@hotmail.com>
2023-11-11 15:41:38 +0100

    chicken and main character (pepe) walk animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,32 @@
 class character extends MovableObject {
-height=250;
-width=100;
-y=180;
+    height = 250;
+    width = 100;
+    y = 180;
+    images_walking = [
+        ' img/2_character_pepe/2_walk/W-21.png',
+        ' img/2_character_pepe/2_walk/W-22.png',
+        ' img/2_character_pepe/2_walk/W-23.png',
+        ' img/2_character_pepe/2_walk/W-24.png',
+        ' img/2_character_pepe/2_walk/W-25.png',
+        ' img/2_character_pepe/2_walk/W-26.png',
+    ];
+   
 
-constructor(){
-    super().loadImage('img/2_character_pepe/2_walk/W-21.png')
-    
-}
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png')
+        this.loadImages(this.images_walking);
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i= this.currentimage % this.images_walking.length;
+            let path = this.images_walking[i];
+            this.img = this.imagecache[path];
+            this.currentimage++;
+        }, 200);
+    }
 
     jump() {
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,22 +1,28 @@
-class MovableObject{
-    x=120;
-    y=250;
-    height= 150;
-    width=100;
+class MovableObject {
+    x = 120;
+    y = 250;
+    height = 150;
+    width = 100;
     img;
-
-
+    imagecache = {};
+    currentimage = 0;
 
     loadImage(path) {
-        this.img= new Image();
-        this.img.src= path;
-            }
-
-    moveRight(){
+        this.img = new Image();
+        this.img.src = path;
+    }
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imagecache[path]=img;
+        });
+    }
+    moveRight() {
 
     }
 
-    moveLeft(){
+    moveLeft() {
 
     }
 }
